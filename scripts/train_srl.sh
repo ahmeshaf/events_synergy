336fccc2dc02e80e1fc93a3c943b1078a2f648fe
rehan <ahmeshaf.rehan@gmail.com>
2024-06-28 16:17:07 -0600

    adding command line args

--- scripts/train_srl.sh
+++ scripts/train_srl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 CONFIG_PATH=./events_synergy/configs/training/t5_train.json
-DATASETS=cu-kairos/propbank_srl_seq2seq
-OUTPUT_DIR=./outputs/srl/propbank_srl_seq2seq
+DATASETS=$1
+OUTPUT_DIR=$2
 
 python -m events_synergy.trainers.multi_task_trainer \
           $CONFIG_PATH \
