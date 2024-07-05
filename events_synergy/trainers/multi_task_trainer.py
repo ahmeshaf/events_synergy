9d96192c53d3fe21b5d387f89ed8a17a78190393
rehan <ahmeshaf.rehan@gmail.com>
2024-07-05 14:17:55 -0600

    update config

--- events_synergy/trainers/multi_task_trainer.py
+++ events_synergy/trainers/multi_task_trainer.py
@@ -275,7 +275,7 @@ def trainer_seq2seq_multi(
         model.print_trainable_parameters()
     else:
         model = AutoModelForSeq2SeqLM.from_pretrained(
-            model_name_or_path, device_map="auto"
+            model_name_or_path, device_map="balanced"
         )
 
     training_args = Seq2SeqTrainingArguments(**config["trainer"])
